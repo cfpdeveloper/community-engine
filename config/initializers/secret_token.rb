4c2123cc15b7e820a0e05232665bf47fbe2be546
cfpdeveloper <admin@crowdfundingpro.com>
2012-07-28 02:04:25 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Community::Application.config.secret_token = 'eda1d8c9d7e2f592165a53c8a036b0d1127016a01b5445bf30a4cd3007f0bb1526701c8cb463896df782090811eb0db4c41db9484e9d8466cd8904ba06083482'
